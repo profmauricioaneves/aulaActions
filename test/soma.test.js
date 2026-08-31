abc8c3df2d097410569488e011ea42eafb46bb89
profmauricioaneves <143768115+profmauricioaneves@users.noreply.github.com>
2026-08-30 21:33:28 -0300

    Update soma.test.js

--- test/soma.test.js
+++ test/soma.test.js
@@ -1,7 +1,7 @@
 const soma = require('../src/soma');
 
 test('soma 2 + 3 deve ser 5', () => {
-  expect(soma(2, 3)).toBe(5);
+  expect(soma(2, 3)).toBe(6);
 });
 
 test('soma de números negativos', () => {
